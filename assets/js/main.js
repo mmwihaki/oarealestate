3fb87d5198175dfaa16128cfe550cc7834157741
mmwihaki <marymwihaki16@gmail.com>
2020-11-18 10:19:22 +0300

    merge files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,8 @@
 /**
-* Template Name: EstateAgency - v2.1.0
-* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
 */
 (function($) {
   "use strict";
 
-  // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
 
   // Back to top button
   $(window).scroll(function() {
